09c3238f4950f4739d8091fa291edb83133afd74
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-01-02 21:23:42 +0900

    연습

---
+++ 4-1.py
@@ -0,0 +1,9 @@
+a = 'Hello Python'
+print(a)
+#length
+print(len(a))
+#index
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[2:5])
---
+++ 4-2.py
@@ -0,0 +1,5 @@
+name = 'egoing' #변수
+print('to '+name+'. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '+name+' incididunt ut labore et dolore magna aliqu. Ut enim ad minim veniam, '+name+' quis nostrud '+name+' exercitation ullamco laboris nisi ut aliquip apple ex ea commodo consequat. '+name+' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '+name+' Excepteur sint occaecat cupidatat apple computer non proident, sunt in culpa qui officia '+name+' deserunt mollit anim id est laborum. '+name+'')
+
+print('+name+')
+#변수는 아래 문장만 되는 거 같음...
