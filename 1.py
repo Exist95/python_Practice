20d54dfa862d7970eea440d08619416aa8f7841d
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-01-21 15:49:02 +0900

    20220121

--- 1.py
+++ 1.py
@@ -1,17 +1,2 @@
-print(-1)
-print(0)
-print(1) #int
-print(1.1) #float
-print('1+1', 1+1)
-print('2-1', 2-1)
-print('2*2', 2*2)
-print('4/2', 4/2) 
-#연산자, 오퍼레이터
-
-import math
-print('math.sqrt(8)', math.sqrt(8))
-print('math.pow(4,2)', math.pow(4,2))
-
-import random
-print('random.random()', random.random())
-
+T = int(input())
+print(T + (10*T + T) + (100*T + 10*T + T) + (1000*T + 100*T + 10*T + T))
